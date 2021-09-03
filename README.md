51ac205fb19b3f5a029203f91273c4df31b20311
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-09-03 20:34:29 +1200

    Update dunstun.py
    Remove checkSite()
    Add getSiteResponse()

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 A simple web scraping twitter bot for good weather in Dunedin, New Zealand. I used the python-twitter package to interface with the Twitter API.
 
 The twitter bot can be found here: [twitter.com/dunstunBot](https://twitter.com/dunstunBot)
+
+## Defining a dunner stunner 
+After countless hours of research and specialist training sitting outside in the sun, this is my definition of a dunner stunner.
+
+- Firstly, it must not be raining during a dunner stunner.
+- Secondly, a dunner stunner must have an outside temperature above a certain value.
+- Finally, the outside temperature of a dunner stunner is dependent on the season.
+
+|  Season  | Base Temperature |
+| -------- |:----------------:|
+|  Summer  |      20&deg      |
+|  Autumn  |      15&deg      |
+|  Winter  |      14&deg      |
+|  Spring  |      15&deg      |
--- dunstun.py
+++ dunstun.py
@@ -1,35 +1,24 @@
-import requests, time, twitter, sys
-
-
-## Add checkSite comment here.
-
-# if sunny and warm, return message about current temp and conditions, else
-# return None.
-def checkSite():
-    URL = "https://www.metservice.com/towns-cities/locations/dunedin"
+import requests, time, twitter, sys, json
+    
+def getSiteResponse():
+    """Return the three hour forecast from metservice.co.nz."""
+    URL = "http://metservice.com/publicData/"
+    
+    URL = ''.join([URL, "localObs_", "dunedin"])
     try:
         page = requests.get(URL)
     except:
         print("The webpage is down.")
         return
 
-    text = page.text
-    start = "" # edit
-    end = "" # edit
-    size = 0 # edit
-    status_start_index = text.index(start)
-    status_end_index = text.index(end, status_start_index)
-    status = text[status_start_index+size:status_end_index]
-    current_temp = float(status)
-    return current_temp
+    return json.loads(page.text)["threeHour"]
 
-# If between daylight hours, sleep for one hour, else sleep until
-# morning.
 if __name__ == "__main__":
+
     api = twitter.Api(consumer_key='blank', consumer_secret='blank',
         access_token_key='blank', access_token_secret='blank')
 
-    # The first command line argument can be used to tweet something out.
+    # The first command line argument can be used to send a tweet.
     if len(sys.argv) == 2:
         tweet = sys.argv[1].strip()
         print("Tweeting tweet:")
@@ -37,10 +26,10 @@ if __name__ == "__main__":
         api.PostUpdate(tweet)
 
     while True:
-
-        response = checkSite()
-        if response is not None:
+        response = getSiteResponse()
+        if int(response["temp"]) > 15 and float(response["rainfall"]) == 0.0:
+            tweet = time.asctime() + " It's a dunner stunner today! A nice " + response["temp"] + "° outside."
             print("Tweeting tweet:")
             print(tweet)
             api.PostUpdate(tweet)
-            time.sleep(3600)
+        time.sleep(60)
