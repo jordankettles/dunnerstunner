1c2568a6c33e4830b3acd2b799cc162eee5fe6bc
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-09-06 12:54:18 +1200

    Update dunstun.py
    Add Seasons

--- dunstun.py
+++ dunstun.py
@@ -1,4 +1,22 @@
 import requests, time, twitter, sys, json
+from datetime import date
+
+def season(some_date):
+    """Returns the current season."""
+    autumn = range(80, 172) # 21 March - 20 June
+    winter = range(173, 266) # 21 June - 22 September
+    spring = range(267, 256) # 23 September - 21 December
+    # Summer is anything else.
+    day = some_date.timetuple().tm_yday
+    if day in autumn:
+        return "autumn"
+    elif day in winter:
+        return "winter"
+    elif day in spring:
+        return "spring"
+    else:
+        return "summer"
+    
     
 def getSiteResponse():
     """Return the three hour forecast from metservice.co.nz."""
@@ -26,10 +44,24 @@ if __name__ == "__main__":
         api.PostUpdate(tweet)
 
     while True:
+        current_season = season(date.today())
         response = getSiteResponse()
-        if int(response["temp"]) > 15 and float(response["rainfall"]) == 0.0:
-            tweet = time.asctime() + " It's a dunner stunner today! A nice " + response["temp"] + "° outside."
-            print("Tweeting tweet:")
-            print(tweet)
-            api.PostUpdate(tweet)
+
+        if current_season == "summer":
+            base_temp = 20
+        elif current_season == "autumn":
+            base_temp = 15
+        elif current_season == "winter":
+            base_temp = 14
+        elif current_season == "spring":
+            base_temp = 15
+
+        if int(response["temp"]) >= base_temp and float(response["rainfall"]) == 0.0:
+            tweet = time.asctime() + " It's a dunner stunner today! A beautiful " + response["temp"] + "° outside."
+        else:
+            tweet = time.asctime() + " It's " + response["temp"] + "° outside in Dunedin today."
+
+        print("Tweeting tweet:")
+        print(tweet)
+        # api.PostUpdate(tweet)
         time.sleep(60)
